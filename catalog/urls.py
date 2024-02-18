15f1f5e4c16fbda3a59d7ea9eb32b40a0699f348
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2024-02-18 23:05:19 +0700

    make hw 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,4 +8,5 @@ app_name = "catalog"
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('products/<int:pk>/', views.detail_view, name='detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
-def index(reqest):
+def index(request):
+    products = Product.objects.all()
     context = {
         'title': 'home',
+        'object_list': products,
     }
-    return render(reqest, 'catalog/index.html', context=context)
+    return render(request, 'catalog/index.html', context=context)
 
 
-def about(reqest):
+def detail_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'object': product,
+    }
+    return render(request, 'catalog/detail_view.html', context=context)
+
+
+def about(request):
     context = {
         'title': 'about',
     }
-    return render(reqest, 'catalog/about.html', context=context)
+    return render(request, 'catalog/about.html', context=context)
